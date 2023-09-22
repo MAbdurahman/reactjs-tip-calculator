6b7ee8e1c365e161a426f40422385baf4296db56
MAbdurahman <mdbdrrhm@gmail.com>
2023-09-22 05:40:56 -0500

    Remove unwanted files and images; add basic structure of project

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import TipCalculator from "../components/TipCalculator";
+
+export default function App() {
+
+    return (
+        <div>
+            <TipCalculator />
+        </div>
+
+    );
+};
---
+++ src/components/TipCalculator.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function TipCalculator() {
+
+    return (
+        <div>
+            <h2>Tip Calculator</h2>
+            <input type="text"/>
+        </div>
+
+    );
+};
